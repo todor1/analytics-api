be3c119dde4431f3d7091b0db7e32de5c19559ad
Justin Mitchel <jmitchel3@users.noreply.github.com>
2025-03-19 14:43:37 -0600

    7 - POST Method to Send our API Data

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -2,8 +2,9 @@ from fastapi import APIRouter
 from .schemas import EventSchema, EventListSchema
 router = APIRouter()
 
-
-# /api/events/
+# Get data here
+# List View
+# GET /api/events/
 @router.get("/")
 def read_events() -> EventListSchema:
     # a bunch of items in a table
@@ -14,8 +15,33 @@ def read_events() -> EventListSchema:
         "count": 3
     }
 
+# SEND DATA HERE
+# create view
+# POST /api/events/
+@router.post("/")
+def create_event(data:dict = {}) -> EventSchema:
+    # a bunch of items in a table
+    print(type(data))
+    return {"id": 123}
 
+
+# GET /api/events/12
 @router.get("/{event_id}")
 def get_event(event_id:int) -> EventSchema:
     # a single row
     return {"id": event_id}
+
+
+# Update this data
+# PUT /api/events/12
+@router.put("/{event_id}")
+def update_event(event_id:int, payload:dict={}) -> EventSchema:
+    # a single row
+    return {"id": event_id}
+
+
+
+# @router.delete("/{event_id}")
+# def get_event(event_id:int) -> EventSchema:
+#     # a single row
+#     return {"id": event_id}
