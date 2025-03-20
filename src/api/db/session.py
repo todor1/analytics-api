ba64a28a83220d899bc1eeae64e72cff559ea761
Justin Mitchel <jmitchel3@users.noreply.github.com>
2025-03-20 11:37:50 -0600

    6 - Create Database Tables with FastAPI Lifespan

---
+++ src/api/db/session.py
@@ -0,0 +1,13 @@
+import sqlmodel
+from sqlmodel import SQLModel
+from .config import DATABASE_URL
+
+if DATABASE_URL == "":
+    raise NotImplementedError("DATABASE_URL needs to be set")
+
+engine = sqlmodel.create_engine(DATABASE_URL)
+
+
+def init_db():
+    print("creating database")
+    SQLModel.metadata.create_all(engine)
--- src/main.py
+++ src/main.py
@@ -1,11 +1,23 @@
+from contextlib import asynccontextmanager
 from typing import Union
 
 from fastapi import FastAPI
+from api.db.session import init_db
 from api.events import router as event_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # before app startup up
+    init_db()
+    yield
+    # clean up
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(event_router, prefix='/api/events')
 # /api/events
+
 
 @app.get("/")
 def read_root():
